a8f66f6614a49a142109e842f3e665d47eeff633
David Axelrod <daxaxelrod@gmail.com>
2016-09-11 23:19:27 -0400

    completed pomodoro

---
+++ pymodoro.py
@@ -0,0 +1,121 @@
+__author__ = 'David'
+
+import tkinter
+from tkinter import messagebox
+
+DEFAULT_GAP = 60 * 25
+
+DEFAULT_GAP = 5
+
+class Pymodoro:
+    def __init__(self, master):
+        self.master = master
+        self.mainframe = tkinter.Frame(self.master, bg='white')
+        self.mainframe.pack(fill=tkinter.BOTH, expand=True)
+
+        self.timer_text = tkinter.StringVar()
+        self.timer_text.trace('w', self.build_timer)
+        self.time_left = tkinter.IntVar()
+        self.time_left.set(DEFAULT_GAP)
+        self.time_left.trace('w', self.alert)
+        self.running = False
+
+        self.build_grid()
+        self.build_banner()
+        self.build_buttons()
+        self.build_timer()
+        self.update()
+
+    def build_grid(self):
+        self.mainframe.columnconfigure(0, weight=1)
+        self.mainframe.rowconfigure(0, weight=0)
+        self.mainframe.rowconfigure(1, weight=1)
+        self.mainframe.rowconfigure(2, weight=0)
+
+    def build_banner(self):
+        banner = tkinter.Label(
+            self.mainframe,
+            background="red",
+            fg="white",
+            text="Pymodoro",
+            font=("Helvetica", 24),
+
+        )
+        banner.grid(
+            row=0, column=0,
+            sticky="ew",
+            padx=10, pady=10,
+        )
+    def build_buttons(self):
+        buttons_frame= tkinter.Frame(self.mainframe)
+        buttons_frame.grid(row=2,column=0, sticky="nsew", padx=10, pady=10)
+        buttons_frame.columnconfigure(0, weight=1)
+        buttons_frame.columnconfigure(1, weight=1)
+
+        self.start_button = tkinter.Button(
+            buttons_frame,
+            text="start",
+            command=self.start_timer
+        )
+        self.stop_button = tkinter.Button(
+            buttons_frame,
+            text="stop",
+            command = self.stop_timer
+        )
+        self.start_button.grid(row=0, column=0, sticky="ew")
+        self.stop_button.grid(row=0, column=1, sticky="ew")
+        self.stop_button.config(state=tkinter.DISABLED)
+
+    def build_timer(self, *args):
+        timer = tkinter.Label(
+            self.mainframe,
+            text=self.timer_text.get(),
+            font=("Helvetica", 48)
+        )
+        timer.grid(row=1, column=0, sticky="nsew")
+
+    def start_timer(self):
+        self.stop_button.config(state=tkinter.NORMAL)
+        self.start_button.config(state=tkinter.DISABLED)
+        self.time_left.set(DEFAULT_GAP)
+        self.running = True
+        print("start")
+
+    def stop_timer(self):
+
+        self.stop_button.config(state=tkinter.DISABLED)
+        self.start_button.config(state=tkinter.NORMAL)
+        self.running = False
+        print("stop")
+
+    def alert(self, *args):
+        if not self.time_left.get():
+            messagebox.showinfo('Timer Done', "Your timer is up!")
+
+    def minutes_seconds(self, seconds):
+        return seconds // 60, int(seconds%60)
+
+    def update(self):
+        time_left = self.time_left.get()
+        if self.running and time_left:
+            mins, secs = self.minutes_seconds(time_left)
+            self.timer_text.set(
+                '{:0>2}:{:0>2}'.format(mins, secs)
+
+            )
+            self.time_left.set(time_left-1)
+        else:
+            mins, secs = self.minutes_seconds(DEFAULT_GAP)
+            self.timer_text.set(
+                '{:0>2}:{:0>2}'.format(mins, secs)
+
+            )
+            self.stop_timer()
+        self.master.after(1000, self.update)
+
+
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    Pymodoro(root)
+    root.mainloop()
